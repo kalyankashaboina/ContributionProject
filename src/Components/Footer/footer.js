e4141fd9beda4889cf85f3243539e0dfe60136f8
Bakthula Ganapathi <144654058+Ganapathi-525@users.noreply.github.com>
2024-05-18 23:44:54 +0530

    adding footer

---
+++ src/Components/Footer/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./footer.css"
+
+function Footer() {
+  return (
+   <div className='footer'>
+
+       
+    <div className='con1'>
+        <h5>Be the first to know</h5>
+    </div>
+
+   </div>
+  )
+}
+
+export default Footer
